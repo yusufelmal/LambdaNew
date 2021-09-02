1c73194f79e7350c4271b83f404500389441436a
Yusuf Elmal <yusufelmal@gmail.com>
2021-09-02 22:59:58 +0200

    v6

--- src/lambda/LambdaFile07.java
+++ src/lambda/LambdaFile07.java
@@ -1,9 +1,11 @@
 package lambda;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
 public class LambdaFile07 {
     public static void main(String[] args) {
@@ -13,6 +15,28 @@ public class LambdaFile07 {
         } catch (IOException e) {
             e.printStackTrace();
         }
+//Tüm karakterleri büyük harfe dönüştürün ve okuyun
+        try {
+            Files.lines(Paths.get("/Users/yusuf/IdeaProjects/Projects/LambdaNew/src/lambda/ahmet")).map(String::toUpperCase).forEach(t-> System.out.println(t));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//metindeki farklı kelimeleri yazdır
 
+        try {
+            Files.lines(Paths.get("/Users/yusuf/IdeaProjects/Projects/LambdaNew/src/lambda/ahmet"))
+                            .map(t -> t.split(" ")).flatMap(Arrays::stream).map(t->t.replaceAll("\\W", "")).distinct().forEach(System.out::println);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // metinde "e" içeren tüm kelimeleri yazdırın. (sayısınıda yazdırabilirsin)
+
+        try {
+            Files.lines(Paths.get("/Users/yusuf/IdeaProjects/Projects/LambdaNew/src/lambda/ahmet")).map(t->t.split(" ")).flatMap(Arrays::stream).filter(t->t.contains("e")).forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
